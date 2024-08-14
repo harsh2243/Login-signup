373f1500109584da53d8baf8225197a7d52ffa19
harsh2243 <https://harshvardhan.jmt.1807gmail.com>
2024-08-14 23:54:13 +0530

    Initial commit

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='flex justify-center items-center text-black text-3xl h-full'>
+      Welcome to Dashboard!!
+     v
+    </div>
+  )
+}
+
+export default Dashboard;
